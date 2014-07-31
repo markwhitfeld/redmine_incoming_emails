144e7919fbfa6831e332501b1ecbcd5689a290ca
mark.whitfeld <mark.whitfeld@chillisoft.co.za>
2014-07-31 14:30:51 +0200

    Added v0.0.1 plugin code.

---
+++ init.rb
@@ -0,0 +1,31 @@
+# Plugin should support Redmine 1.x and 2.x becuse guidelines were followed from: http://www.redmine.org/boards/3/topics/31445 
+# Support for 1.x has not yet been confirmed. Any takers?
+
+# Patches to the Redmine core.
+require 'dispatcher' unless Rails::VERSION::MAJOR >= 3
+
+if Rails::VERSION::MAJOR >= 3
+  ActionDispatch::Callbacks.to_prepare do
+    require_dependency 'redmine_incoming_emails/patches/mail_handler_patch'
+  end
+else
+  Dispatcher.to_prepare :redmine_incoming_emails do
+    require_dependency 'redmine_incoming_emails/patches/mail_handler_patch'
+  end
+end
+
+# Plugin Definition
+Redmine::Plugin.register :redmine_incoming_emails do
+  name 'Incoming Emails plugin'
+  author 'Mark Whitfeld'
+  description 'This is a plugin for Redmine that allows for the configuration of the default project used when a user logs a new issue using email.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+  
+  # See note at top of file regarding support for 1.x
+  requires_redmine :version_or_higher => '2.0.0'
+  
+  settings(:partial => 'settings/incoming_emails_settings',
+           :default => {} )
+end
---
+++ lib/redmine_incoming_emails/patches/mail_handler_patch.rb
@@ -0,0 +1,36 @@
+require_dependency 'mail_handler'
+
+module IncomingEmails
+  module Patches
+    module MailHandlerPatch
+      def self.included(base) # :nodoc:
+        base.extend(ClassMethods)
+
+        # sending instance methods to module
+        base.send(:include, InstanceMethods)
+        base.class_eval do
+          unloadable
+          
+          alias_method_chain :target_project, :user_specific_defaults
+        end
+      end
+
+      module ClassMethods
+      end
+
+      module InstanceMethods
+        def target_project_with_user_specific_defaults
+          targetProjectId = Setting.plugin_redmine_incoming_emails[user.mail].to_i
+          target = Project.find(targetProjectId) if targetProjectId > 0
+          target = target_project_without_user_specific_defaults if target.nil?
+          return target
+        end
+      end
+    end
+  end
+end
+
+# Include the module if it is not already included
+unless MailHandler.included_modules.include? IncomingEmails::Patches::MailHandlerPatch
+    MailHandler.send(:include, IncomingEmails::Patches::MailHandlerPatch)
+end
